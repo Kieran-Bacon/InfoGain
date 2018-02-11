84012d8a7a32cbe2f14911569bd8a95251e848df
Kieran Bacon <Kieran.Bacon@outlook.com>
2018-02-11 20:29:07 +0000

    Finished ADE parse
    Changes script file to be able to create valid datapoint inputs
    Added print to file functionality

--- scripts/ADE_ConvertDataset.py
+++ scripts/ADE_ConvertDataset.py
@@ -3,45 +3,44 @@ Aim of the script is to convert the ADE dataset into an ontology, and a universa
 file to introduce into the Relation Extraction
 """
 
-import re
-from InfoGain import Ontology, Concept, Relation
+import re, json
+from InfoGain import Ontology
 
-ADE = Ontology("Hey", "./ADE.json")
-
-drugOntology = Ontology("ADE")
-drug, dose, effect = Concept("Drug"), Concept("Dose"), Concept("Adverse Effect")
-
-drugOntology.addConcept(drug)
-drugOntology.addConcept(dose)
-drugOntology.addConcept(effect)
-
-drugOntology.addRelation(Relation({drug},"hasAffect",{effect}))
-drugOntology.addRelation(Relation({drug},"hasDose",{dose}))
+ADE = Ontology(filepath="./ADE_corpus.ont")
 
 with open("./DRUG-DOSE.rel","r") as handler:
     document = handler.read().splitlines()
 
 points = []
 for documentLine in document:
-    line = documentLine[0].split("|")
+    line = documentLine.split("|")
 
     sentence = line[1]
-    domain = line[2]
+    domain = line[5]
+    target = line[2]
+
+    if re.search(domain,sentence) is None: continue
+    if re.search(target,sentence) is None: continue
+
     domain_span = re.search(domain,sentence).span()
-    target = line[5]
+    
     target_span = re.search(target,sentence).span()
 
     first = domain_span if int(domain_span[0]) < int(target_span[0]) else target_span
     second = domain_span if int(domain_span[0]) > int(target_span[0]) else target_span
 
     datapoint = {}
-    datapoint["domain"] = domain
-    datapoint["target"] = target
-    datapoint["leading words"] = sentence[:int(first[0])])
-    datapoint["concept 1"] = sentence[int(first[0]):int(first[1])]
-    datapoint["inter words"] = sentence[int(first[1]):int(second[0])])
-    datapoint["concept 2"] = sentence[int(second[0]):int(second[1])]
-    datapoint["trailing words"] = sentence[int(second[1]):])
+    datapoint["text"] = sentence
+    datapoint["domain"] = {"concept":"Drug", "text":domain}
+    datapoint["target"] = {"concept":"Dose", "text":target}
+    datapoint["context"] = {"left":sentence[:int(first[0])],\
+                            "middle":sentence[int(first[1]):int(second[0])],\
+                            "right":sentence[int(second[1]):]}
+
+    datapoint["relation"] = "has_dosage"
+    datapoint["annotation"] = 1
 
     points.append(datapoint)
-    
+
+with open("./DRUG-DOSE.rel.datapoints", "w") as handler:
+    handler.write(json.dumps({"datapoints":points}, indent=4, sort_keys=True))
