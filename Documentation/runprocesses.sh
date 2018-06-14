14b29c5c24a501c9652d08ddf9bad90b3545d4a0
Kieran Bacon <Kieran.Bacon@outlook.com>
2018-06-14 19:17:29 +0100

    Removed personal work

--- Documentation/runprocesses.sh
+++
@@ -1,5 +0,0 @@
-cd /home/kieran/Projects/InfoGain/Documentation/Datasets/datasets/SemEval2007-Task4
-python3 /home/kieran/Projects/InfoGain/Documentation/Datasets/datasets/SemEval2007-Task4/process.py
-
-cd /home/kieran/Projects/InfoGain/Documentation/Datasets/datasets/ADE-Corpus-V2/
-python3 /home/kieran/Projects/InfoGain/Documentation/Datasets/datasets/ADE-Corpus-V2/process.py
