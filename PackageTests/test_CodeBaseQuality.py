db131e59606dcb6326361a59228aaff312a90bc4
Kieran Bacon <Kieran.Bacon@outlook.com>
2018-11-10 21:34:51 +0000

    improved Code quality test to include passes and work on tests
    Tests are now checked for being of good quality
    Pass statements are now found also aka unhandled errors + unfinished definitions

--- PackageTests/test_CodeBaseQuality.py
+++ PackageTests/test_CodeBaseQuality.py
@@ -6,18 +6,21 @@ class Test_CodeQuality(unittest.TestCase):
 
         self.ignore = [
             "__pycache__",
-            "artefact/__init__.py"
+            "artefact/__init__.py",
+            "infogain/exceptions.py"
         ]
         self.infogain_location = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "infogain"))
+        self.tests_location = os.path.abspath(os.path.dirname(__file__))
 
+    def parse_directory(self, directory):
 
-    def test_Code_to_be_removed(self):
         issues = []
 
         printIssue = "Print statement found in {}: line {} \"{}\""
         todoIssue = "TODO statement found in {}: line {} \"{}\""
+        passIssue = "Pass statement found in {}: line {} \"{}\""
 
-        for (dirpath, _, filenames) in os.walk(self.infogain_location):
+        for (dirpath, _, filenames) in os.walk(directory):
 
             for filename in filenames:
                 directory = dirpath.split("/")[-1]
@@ -27,14 +30,32 @@ class Test_CodeQuality(unittest.TestCase):
 
                 try:
                     with open(os.path.join(dirpath, filename), "r") as handler:
+                        previous = "BEGINNING OF SENTENCE"
                         for line, content in enumerate(handler):
                             content = re.sub("\n","", content.strip())
                             if re.search(r"print\(.*\)", content):
                                 issues.append(printIssue.format(filename, line, content))
                             if re.search(r"#\s*TODO", content):
                                 issues.append(todoIssue.format(filename, line, content))
+                            if re.search(r"^\s*pass", content):
+                                issues.append(passIssue.format(filename, line, previous))
+                            previous = content
                 except:
                     continue
+        
+        return issues
+
+
+    def test_Code_to_be_removed(self):
+
+        issues = self.parse_directory(self.infogain_location)
+
+        if issues:
+            self.fail("\n" + "\n".join(issues))
+
+    def test_tests_Code_to_be_removed(self):
+
+        issues = self.parse_directory(self.tests_location)
 
         if issues:
             self.fail("\n" + "\n".join(issues))
