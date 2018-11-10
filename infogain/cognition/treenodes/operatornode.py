599232f2d041facb1f1e94ddca34437a3f8014c1
Kieran Bacon <Kieran.Bacon@outlook.com>
2018-11-10 02:50:44 +0000

    extend operators and implement necessary eval tree functions

--- infogain/cognition/treenodes/operatornode.py
+++ infogain/cognition/treenodes/operatornode.py
@@ -3,23 +3,32 @@ import re
 from .decorators import scenario_consistent
 from ..evaltrees import EvalTree
 
-def equal(a, b): return a == b
-def notEqual(a, b): return a != b
-
 class OperatorNode(EvalTree):
 
     operators = {
-        "==": equal,
-        "!=": notEqual
+        "==": lambda a, b : a == b,
+        "!=": lambda a, b : a != b,
+
+        ">(?!=)":  lambda a, b : a > b,
+        "<(?!=)":  lambda a, b : a < b,
+        ">=":  lambda a, b : a >= b,
+        "<=":  lambda a, b : a <= b,
+
+        "is(?! not)":  lambda a, b : a is b,
+        "is not": lambda a, b : a is not b,
+        "and":  lambda a, b : a and b,
+        "or":  lambda a, b : a or b,
     }
 
-    expression = re.compile(r".+(?P<operator>{}).+".format("|".join(operators)))
+    expression = re.compile(r".+(?P<operator>{}).+".format("|".join(operators.keys())))
 
     def __init__(self, left: EvalTree, operator: str, right: EvalTree):
         self.left = left
         self.operator = operator
         self.right = right
 
-    @scenario_consistent
+    def __str__(self): return "{}{}{}".format(self.left, self.operator, self.right)
+    def parameters(self): return self.left.parameters().union(self.right.parameters())
+
     def eval(self, **kwargs):
         return self.operators[self.operator](self.left.eval(**kwargs), self.right.eval(**kwargs))
