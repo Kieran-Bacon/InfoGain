b9d4834be2cf409497156a6f6b10a5bce6c3e23f
Kieran Bacon <Kieran.Bacon@outlook.com>
2019-03-07 19:39:26 +0000

    wip

---
+++ infogain/artefact/datacontainer.py
@@ -0,0 +1,12 @@
+from .document import Document
+
+class DataContainer(Document):
+    """ A class to contain datapoints only. To provide no additional utility for documentation content over the 
+    traditional serving of datapoints and content
+
+    Params:
+        
+    """
+
+
+    pass
--- infogain/artefact/document.py
+++ infogain/artefact/document.py
@@ -9,7 +9,7 @@ class Document:  # TODO: Change the method by which concepts and relations are s
     """ A document represents a textual source, a file, paper, etc. The document provides a method
     to manipulate and extract information from the source and provides the method of processing
     the information within and ontology to generate potential datapoints within the source.
-    
+
     Params:
         name (str) - The name of the document, used as the file name if saved
         content (str) - The content of the document as a string
@@ -21,8 +21,8 @@ class Document:  # TODO: Change the method by which concepts and relations are s
 
     @classmethod
     def removeWhiteSpace(cls, raw_string: str):
-        """ Remove the excessive white space within a string 
-        
+        """ Remove the excessive white space within a string
+
         Params:
             raw_string (str) - The raw string to be cleaned
 
@@ -37,14 +37,14 @@ class Document:  # TODO: Change the method by which concepts and relations are s
     @classmethod
     def clean(cls, raw_string: str) -> str:
         """ Cleans a string of invalid and grammatical characters, expands words definitions,
-        corrects spelling. The function cleans and returns a string 
-        
+        corrects spelling. The function cleans and returns a string
+
         Params:
             raw_string (str) - The raw uncleaned string
-            
+
         Returns:
             cleaned (str) - The cleaned string
-        """ 
+        """
 
         cleaned_string = []  # The components of the sentence
         for rawWord in raw_string.split():
@@ -75,11 +75,11 @@ class Document:  # TODO: Change the method by which concepts and relations are s
     def split(cls, text: str, separators: [re]) -> [str]:
         """ Separate the text with the separators that has been given. Replace all
         separators with a single separator and then split by the single separator
-        
+
         Params:
             text - The string to be split
             separators - A list of separator strings to have the string split by
-            
+
         Returns:
             str - A collection of ordered strings representing the initial text split by the
                 separators
@@ -88,7 +88,7 @@ class Document:  # TODO: Change the method by which concepts and relations are s
         if not isinstance(separators, list): separators = [separators]
 
         # Find all the places the text needs to be split
-        splitIndexes = [match.span()[1] for separator in separators 
+        splitIndexes = [match.span()[1] for separator in separators
             for match in separator.finditer(text)]
         splitIndexes = sorted(splitIndexes)
 
@@ -107,7 +107,7 @@ class Document:  # TODO: Change the method by which concepts and relations are s
         self.name = name if name else uuid.uuid4().hex
         self._concepts = None
         self._relations = None
-        
+
         # Save the content, overwrite provided content with read content
         self._content = content
         if filepath:
@@ -129,9 +129,9 @@ class Document:  # TODO: Change the method by which concepts and relations are s
             pass
 
     def __len__(self) -> int:
-        """ Return the assumed length of the document, the number of datapoints, if none give, the 
-        length of the content 
-        
+        """ Return the assumed length of the document, the number of datapoints, if none give, the
+        length of the content
+
         Returns:
             int - The length of the document. Either the size of the document of the number of data
                 points
@@ -150,7 +150,7 @@ class Document:  # TODO: Change the method by which concepts and relations are s
 
         # TODO: Fix this issue - If concepts is generated, further changes to this document do not change this
         if self._concepts is None:
-            self._concepts = {} 
+            self._concepts = {}
 
             for p in self._datapoints:
                 self._concepts[p.domain["concept"]] = self._concepts.get(p.domain["concept"], []) + [p.domain["text"]]
@@ -176,24 +176,23 @@ class Document:  # TODO: Change the method by which concepts and relations are s
         return self._relations
 
 
-
     def text(self) -> str:
         """ Return the entire content of the document, but, doesn't construct the content from data
         points. Only provides content that was provides from source.
-        
+
         Returns:
             str - A string of the contents of the document
         """
         return self._content
- 
+
     def sentences(self, cleaned: bool = False) -> [str]:
-        """ Breaks the contents of the source down into sentences. A toggle to clean the sentences 
-        as they were being returned. If the document doesn't have any source content, then the 
+        """ Breaks the contents of the source down into sentences. A toggle to clean the sentences
+        as they were being returned. If the document doesn't have any source content, then the
         sentences are generated from the data points held by the document.
-        
+
         Params:
             cleaned (bool) - A toggle to clean the contents
-            
+
         Returns:
             sentences ([str]) - A list of strings, each string is a sentence
         """
@@ -202,20 +201,20 @@ class Document:  # TODO: Change the method by which concepts and relations are s
                 return [Document.clean(sen)
                     for sen in Document.split(self._content, Document.SENTENCE)]
             return Document.split(self._content, Document.SENTENCE)
-        
+
         if self._datapoints:
             if cleaned:
                 return [Document.clean(point.text) for point in self._datapoints]
             return [point.text for point in self._datapoints]
 
     def words(self, cleaned: bool = False) -> [[str]]:
-        """ Break down the contents of the source down into collections of words. The collections 
+        """ Break down the contents of the source down into collections of words. The collections
         are of the sentences in the source. Uses the sentences function, and splits the sentences in
         place.
-        
+
         Params:
             cleaned (bool) - A toggle to clean the words
-        
+
         Returns:
             words ([[str]]) - A list of sentences, each sentence is a list of words
         """
@@ -224,7 +223,7 @@ class Document:  # TODO: Change the method by which concepts and relations are s
                 return [Document.clean(sen).split()
                     for sen in Document.split(self._content, Document.SENTENCE)]
             return [sen.split() for sen in Document.split(self._content, Document.SENTENCE)]
-            
+
         if self._datapoints:
             if cleaned:
                 return [Document.clean(point.text).split() for point in self._datapoints]
@@ -232,7 +231,7 @@ class Document:  # TODO: Change the method by which concepts and relations are s
 
     def addDatapoint(self, point: Datapoint) -> None:
         """ Add a datapoint into the document. Extend the content of this document by the contents of the datapoint
-        
+
         Params:
             point (Datapoint): The datapoint object to be added
         """
@@ -260,12 +259,30 @@ class Document:  # TODO: Change the method by which concepts and relations are s
         """ Determine whether the the document has any datapoints """
         return len(self._datapoints) > 0
 
+
+    def analyse(self, ontology: Ontology):
+
+        self.namedEntityRecognition(ontology)
+        self.coreferencing()
+        self.relationExtraction(ontology)
+
+    def namedEntityRecognition(self):
+        """ Perform named entity recognition on the document, and identify a collection of entities that represent the
+        ontology concepts
+        """
+        pass
+
+    def coreferencing(self):
+        """ Perform co-referencing on the document and identify words that have been linked with other entities that
+        """
+        pass
+
     def processKnowledge(self, ontology: Ontology) -> None:
-        """ Iterate over the document and create potential datapoints for all possible combinations 
+        """ Iterate over the document and create potential datapoints for all possible combinations
         of the concepts that are found.
-        
+
         Params:
-            ontology - The ontology object that contains all the information about the concepts and 
+            ontology - The ontology object that contains all the information about the concepts and
                 relationships we care about
         """
 
@@ -281,18 +298,18 @@ class Document:  # TODO: Change the method by which concepts and relations are s
             for pattern in reprMap.keys()]
 
         def createDatapoint(dom, domCon, tar, tarCon, relations, sentence):
-            """ Generate the datapoints and add them to the document datapoint collection 
-            
+            """ Generate the datapoints and add them to the document datapoint collection
+
             Params:
                 dom (re.Match) - The domain match object
                 domCon (Concept) - The domain concept the match represents
                 tar (re.Match) - The target match object
                 tarCon (Concept) - The target match object
-                relations ([Relation]) - All relations that can be formed between the domain and 
+                relations ([Relation]) - All relations that can be formed between the domain and
                     target
                 sentence (str) - The sentence that this information originated from
-            """ 
-            
+            """
+
             p1, p2 = sorted((dom.span(), tar.span()))  # Sort the spans of the elements
 
             for relation in relations:
@@ -329,7 +346,7 @@ class Document:  # TODO: Change the method by which concepts and relations are s
 
                     # For each combination of elements within the sentence
                     for instConcept, matchConcept in itertools.product(instConcepts, matchConcepts):
-                    
+
                         # Create the datapoints for the document
                         relations = ontology.findRelations(domain=instConcept, target=matchConcept)
                         [self._datapoints.append(p) for p in createDatapoint(inst_match,
@@ -345,7 +362,7 @@ class Document:  # TODO: Change the method by which concepts and relations are s
 
         Params:
             meta_only (bool): clone this document's meta data only
-        
+
         returns:
             Document:
         """
@@ -358,7 +375,7 @@ class Document:  # TODO: Change the method by which concepts and relations are s
             return cloned
 
     def save(self, folder: str = "./", filename: str = None) -> None:
-        """ Save the training file and it's datapoints, checks to see if the location is a file or a 
+        """ Save the training file and it's datapoints, checks to see if the location is a file or a
         directory. If directory, the file will be saved as the name of the document currently
         set.
 
@@ -366,14 +383,13 @@ class Document:  # TODO: Change the method by which concepts and relations are s
             location - The location of where the training document is to be saved
         """
 
-        if filename is None: filename = self.name
-            
-        struct = {
-            "name": self.name,
-            "content": self._content,
-            "datapoints": [point.minimise() for point in self._datapoints]
-        }
-        
-        path = os.path.join(folder, filename)
-        with open(path, "w") as filehandler:
-            filehandler.write(json.dumps(struct, indent=4))
+        path = os.path.join(folder, filename or self.name)
+        with open(path, "w") as handler:
+            handler.write(
+                json.dumps({
+                    "name": self.name,
+                    "content": self._content,
+                    "datapoints": [point.minimise() for point in self._datapoints]
+                }),
+                indent = 4
+            )
