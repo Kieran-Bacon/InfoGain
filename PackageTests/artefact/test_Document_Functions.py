7e2b1e3189521ddac0d83a238c946e8602813a3b
Kieran Bacon <kieran.bacon@outlook.com>
2019-09-12 21:29:23 +0100

    remove the annotation function for documents

--- PackageTests/artefact/test_Document_Functions.py
+++ PackageTests/artefact/test_Document_Functions.py
@@ -1,7 +1,7 @@
 import unittest, sys
 from unittest import mock
 
-from infogain.artefact import Document, Annotation, score, annotate
+from infogain.artefact import Document, Annotation, score
 from infogain.resources.ontologies import language
 
 class Test_Document_Functions(unittest.TestCase):
@@ -26,15 +26,3 @@ class Test_Document_Functions(unittest.TestCase):
         self.assertEqual(2/3, corpus["precision"])
         self.assertEqual(1, corpus["recall"])
         self.assertAlmostEqual(0.8, corpus["f1"])
-
-    def test_annotate_function(self):
-        """ Test the annotation function """
-
-        document = Document(content="Luke can speak English rather well, but Luke doesn't live in England.")
-
-        with mock.patch('builtins.input', side_effect=["N"] + ["0"]*7):
-            annotate(self.ont_language, document)
-
-        self.assertTrue(len(document) == 7)
-        for point in document.datapoints():
-            self.assertTrue(point.annotation == 0)
